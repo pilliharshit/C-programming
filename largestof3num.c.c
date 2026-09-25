4194bc3f99f5ab4b53b75b69333fae6a6061901d
pilliharshit <pilli.harshit7@gmail.com>
2026-09-25 15:33:44 +0530

    Add files via upload

---
+++ largestof3num.c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int  main()
+{
+    int a,b,c;
+    printf("Enter a,b,c values:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>=b && a>=c)
+    {
+        printf("%d is the largest  number",a);
+    }
+    else if(b>=a && b>=c)
+    {
+        printf("%d is the largest number",c);
+    }
+    else
+    {
+        printf("%d is the largest number",c);
+    }
+}
